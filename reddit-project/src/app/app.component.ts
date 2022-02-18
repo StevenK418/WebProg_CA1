3e447aba4109c46c7077abb0655c94356ddc0565
StevenK418 <stevenkelly418@gmail.com>
2022-02-18 12:25:25 +0000

    => Added array of articles to app component.

--- reddit-project/src/app/app.component.ts
+++ reddit-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent 
 {  
+  articles:Article[];
+
+  constructor()
+  {
+    this.articles = 
+    [
+      new Article("Angular", "http://angular.io", 5),
+      new Article("Google", "http://google.ie", 3),
+      new Article("TikTok", "http://tiktok.com", 1)
+    ];
+  }
+
   addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean
   {
+    this.articles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
+
     console.log(`Adding Article Title ${title.value} : and adding Article Link ${link.value}`);
     return false;
   }
--- reddit-project/src/app/article/article.component.ts
+++ reddit-project/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,10 +8,11 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
 
-  article:Article;
+  
+  @Input() article:Article;
 
   constructor() { 
-    this.article = new Article('Angular', 'Http://angular.io', 10);
+    //this.article = new Article('Angular', 'Http://angular.io', 10);
   }
 
   ngOnInit(): void {
