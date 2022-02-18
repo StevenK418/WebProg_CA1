3d5d8097b2823410751a1bf22bf69699dfbf9ea6
StevenK418 <stevenkelly418@gmail.com>
2022-02-18 11:05:58 +0000

    => Added addArticle functionality.

--- reddit-project/src/app/app.component.ts
+++ reddit-project/src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reddit-project';
+export class AppComponent 
+{  
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean
+  {
+    console.log(`Adding Article Title ${title.value} : and adding Article Link ${link.value}`);
+    return false;
+  }
 }
