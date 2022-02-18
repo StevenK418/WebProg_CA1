abe96263256da71c0f20b3a8b3e2913f20a47f1d
StevenK418 <stevenkelly418@gmail.com>
2022-02-18 11:27:13 +0000

    => Added article component and functionality.

---
+++ reddit-project/src/app/article/article.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  votes:number;
+  title:string;
+  link:string
+
+  constructor() { 
+    this.title = "Angular";
+    this.link = "http://angular.io";
+    this.votes = 10;
+  }
+
+  ngOnInit(): void {
+  }
+
+  voteUp():boolean
+  {
+    this.votes++;
+    return false;
+  }
+
+  voteDown():boolean
+  {
+    this.votes--;
+    return false;
+  }
+
+}
